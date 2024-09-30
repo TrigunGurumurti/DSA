8efea94e65bf8ca4e38a823bccb44ddd7fb0938b
TrigunGurumurti <triguntamragouri@gmail.com>
2024-09-30 19:53:35 +0530

    stringsBasics

---
+++ Basics/strings.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str="Trigun";
+    cout<<str.size()<<endl; // 6
+    int length=str.size();
+    for(int i=0;i<=length-1;i++)
+    {
+        cout<<str[i]<<" ";
+    }
+    return 0;
+}
