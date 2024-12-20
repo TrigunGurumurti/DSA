4ed8818bc0e9c031ea40a1c1240fe9e38ac65eb5
TrigunGurumurti <triguntamragouri@gmail.com>
2024-12-20 20:08:31 +0530

    Basics of String

---
+++ Basics/string.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str); // picks up the entire string present in a line
+    cout << str;
+    return 0;
+}
