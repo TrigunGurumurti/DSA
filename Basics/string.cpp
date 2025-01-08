c3e970d5fcf84b5b751292ede565d2fa72e032cd
TrigunGurumurti <triguntamragouri@gmail.com>
2025-01-08 21:06:14 +0530

    string

--- Basics/string.cpp
+++ Basics/string.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    string str;
+    string str; // string is a class under namespace std
     getline(cin, str); // picks up the entire string present in a line
     cout << str;
     return 0;
